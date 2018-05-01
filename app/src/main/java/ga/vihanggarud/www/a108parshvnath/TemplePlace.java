0a7be07ebff961d6798f511bc19d421a10b0895f
adityavikramthampi <adityavikram.thampi@gmail.com>
2018-05-01 14:51:44 +0530

    Added temple place activity.

---
+++ app/src/main/java/ga/vihanggarud/www/a108parshvnath/TemplePlace.java
@@ -0,0 +1,13 @@
+package ga.vihanggarud.www.a108parshvnath;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TemplePlace extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_temple_place);
+    }
+}
