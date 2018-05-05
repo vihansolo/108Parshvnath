e391e66cc7463c6fbe5cd7ed4d296f2d7c900571
Vihang Garud <garudvihang0@gmail.com>
2018-05-05 22:42:31 +0530

    Added maps navigation.

--- app/src/main/java/ga/vihanggarud/www/a108parshvnath/TempleDetails.java
+++ app/src/main/java/ga/vihanggarud/www/a108parshvnath/TempleDetails.java
@@ -1,8 +1,10 @@
 package ga.vihanggarud.www.a108parshvnath;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -45,5 +47,15 @@ public class TempleDetails extends AppCompatActivity {
 
         templeName.setText(name);
         templeAddress.setText(address);
+    }
+
+    public void onViewOnMapClicked (View view) {
+
+        Uri goToMapUri = Uri.parse("google.navigation:q="+latLng.latitude+","+latLng.longitude);
+
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, goToMapUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+
+        startActivity(mapIntent);
     }
 }
