b3d1fc5a150aa12ea6a8a9b921c360e5870dc71f
Vihang Garud <garudvihang0@gmail.com>
2018-05-05 21:52:24 +0530

    Added working temple details activity.

--- app/src/main/java/ga/vihanggarud/www/a108parshvnath/TemplePlace.java
+++ app/src/main/java/ga/vihanggarud/www/a108parshvnath/TemplePlace.java
@@ -1,13 +1,49 @@
 package ga.vihanggarud.www.a108parshvnath;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.maps.model.LatLng;
 
 public class TemplePlace extends AppCompatActivity {
+
+    LatLng latLng;
+    ImageView templeImage;
+    TextView templeName;
+    TextView templeAddress;
+
+    String name;
+    String address;
+    String imageURI;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_temple_place);
+
+        templeName = findViewById(R.id.templeName);
+        templeImage = findViewById(R.id.templeImage);
+        templeAddress = findViewById(R.id.templeAddress);
+
+        Intent intent = getIntent();
+
+        latLng = new LatLng(intent.getDoubleExtra("latitude",19.0639001),
+                intent.getDoubleExtra("longitude",72.8349828));
+        name = intent.getStringExtra("templeName");
+        address = intent.getStringExtra("templeAddress");
+        imageURI = intent.getStringExtra("templeImageURI");
+
+        imageURI = imageURI.substring(1,imageURI.length());
+
+        Glide.with(templeImage.getContext())
+                .load(imageURI)
+                .into(templeImage);
+
+        templeName.setText(name);
+        templeAddress.setText(address);
     }
 }
