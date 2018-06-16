eac024d50829c6b6220c55223f9d2928a431a31e
Vihang Garud <garudvihang0@gmail.com>
2018-06-17 00:58:33 +0530

    All Done (Hopefully).

--- app/src/main/java/ga/vihanggarud/www/a108parshvnath/NearbyPlaces.java
+++ app/src/main/java/ga/vihanggarud/www/a108parshvnath/NearbyPlaces.java
@@ -1,9 +1,14 @@
 package ga.vihanggarud.www.a108parshvnath;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -32,6 +37,8 @@ public class NearbyPlaces extends AppCompatActivity {
     List<Temple> nearbyTemples = new ArrayList<>();
     ItemAdapter itemAdapter;
 
+    static final int REQUEST_LOCATION = 1;
+    LocationManager locationManager;
     double userLatitude;
     double userLongitude;
 
@@ -39,6 +46,9 @@ public class NearbyPlaces extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nearby_places);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        getLocation();
 
         nearbyPlacesListView = findViewById(R.id.nearby_places_list);
 
@@ -100,6 +110,44 @@ public class NearbyPlaces extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void getLocation() {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+        }
+
+        else {
+
+            Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+            if (location != null) {
+
+                userLatitude = location.getLatitude();
+                userLongitude = location.getLongitude();
+            }
+
+            else
+                Toast.makeText(this, "Please Accept Permissions", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+
+            case REQUEST_LOCATION :
+                getLocation();
+                break;
+        }
     }
 
     public static double getDistance(double lat1, double lng1, double lat2, double lng2) {
